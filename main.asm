6ddaea8f29679035a2a2b48e0803d3983464085c
Vazquez Joel <jbcv19@gmail.com>
2018-02-12 20:35:27 -0800

    master template for 32bit program

--- main.asm
+++ main.asm
@@ -1,29 +1,45 @@
-;Program Description:		
-;Author:			Joel Vazquez
-;Creation Date:		
-;Revisions:			
-;Date:				
-;Operating Sys:		Linux Mint
-;IDE/Compiler		IDE: N/A	Compiler: 
+;Program Description:
+;Author:
+;Creation Date:
+;Revisions:
+;Date:			Modified by:
+;Date:
+;Operating System:
+;IDE/Compiler
 
-		
+%include "./functions.inc"
+
 bits 32
 section .data
-	;init data definitions here
-	
+  ;Initialized data definitions go here
+  
+  welcomePrompt		db		"welcome to my program", 0dh, 0ah, 0h
+  inputPrompt		db		"Enter an unsigned int:", 0h
+
 section .bss
-	;uniti memory reservations here
+  ;Uninitialized memory reservations go here
 
 section .text
 
 global _start
 _start:
-	nop
-	;code start
-	
-	;code end
-	nop
-	
-	mov eax,1		;exit system call
-	mov ebx,0		;exit return code
-	int 80h			;call kernel
+  nop
+  ;Code starts here
+  
+  push		welcomePrompt
+  call		PrintString
+  call		Printendl
+  
+  push		inputPrompt
+  call		PrintString 
+  call		InputUInt
+  call		Printendl
+  
+  call 		PrintRegisters
+  
+
+  ;Code ends here
+  nop
+  mov eax,1 ; Exit system call value
+  mov ebx,0 ; Exit return code
+  int 80h ; Call the kernel
