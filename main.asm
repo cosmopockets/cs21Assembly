eab808dbaa8c8c13d14717e5a864f034cf1aee15
Vazquez Joel <jbcv19@gmail.com>
2018-02-19 20:51:57 -0800

    attempted to complete usingonly eax register

--- main.asm
+++ main.asm
@@ -13,7 +13,7 @@ bits 32
 section .data
   ;Initialized data definitions go here
   
-  ;welcomePrompt		db		"This asm program will do the following A+(B+C)=D & (A+C)-B=D", 0dh, 0ah, 0h
+;  welcomePrompt		db		"This asm program will do the following A+(B+C)=D & (A+C)-B=D", 0dh, 0ah, 0h
   varA				db		10h				;this is the single byte variable
   varB				dw		2000h			;this is the word variable
   varC				dd		30000h			;this is the variable for the double word
@@ -31,24 +31,33 @@ _start:
   nop
   ;Code starts here
   
-  ;push		welcomePrompt
-  ;call		PrintString
-  ;call		Printendl
+;  push		welcomePrompt
+;  call		PrintString
+;  call		Printendl
   
   
- ; call 		PrintRegisters
+;  call 		PrintRegisters
   
   ;A+(B+C) =D
   
   mov	eax, 0h					;zero out the eax register
-  mov	eax,[varB]				;we move the 2 byt value of varB in ax register
   
-  mov	ebx, 0h					;we need to zero out the ebx register since varC & D ar 4 bytes
-  mov	ebx, [varC]				;mov the value of varC into the ebx register
+  mov	al,[varA]				;move the single byte register into the ax subregister
+  mov	ax,[varB]				;we move the 2 byt value of varB in ax register
+  mov 	eax,[varC]
+  ;mov	ebx,0h					;we need to zero out the ebx register since varC & D ar 4 bytes
+  ;mov	ebx,[varC]				;mov the value of varC into the ebx register
   
-  add	eax,ebx					;we add ebx TO eax because we want all math operations to 
-								;finish inside of the eax register
+  ;add	eax,ebx					;we add ebx TO eax because we want all math operations to 
+  
+  ;add 	eax,al
+  
+  add	eax,ax					;add ax [varC] to eax [varB]
+  
+  add 	eax,al					;add [var1] to eax
+  
 								
+  					
   ;Code ends here
   nop
   mov eax,1 ; Exit system call value
