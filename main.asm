ad89e831df0e0b8a4ba9acc0363d0137bb5ebb4e
Vazquez Joel <jbcv19@gmail.com>
2018-02-19 17:08:38 -0800

    added [] around var's so they work

--- main.asm
+++ main.asm
@@ -13,11 +13,16 @@ bits 32
 section .data
   ;Initialized data definitions go here
   
-  welcomePrompt		db		"welcome to my program", 0dh, 0ah, 0h
-  inputPrompt		db		"Enter an unsigned int:", 0h
+  ;welcomePrompt		db		"This asm program will do the following A+(B+C)=D & (A+C)-B=D", 0dh, 0ah, 0h
+  varA				db		10h				;this is the single byte variable
+  varB				dw		2000h			;this is the word variable
+  varC				dd		30000h			;this is the variable for the double word
+    
 
 section .bss
   ;Uninitialized memory reservations go here
+  
+  varD		resd		4	
 
 section .text
 
@@ -26,18 +31,24 @@ _start:
   nop
   ;Code starts here
   
-  push		welcomePrompt
-  call		PrintString
-  call		Printendl
+  ;push		welcomePrompt
+  ;call		PrintString
+  ;call		Printendl
   
-  push		inputPrompt
-  call		PrintString 
-  call		InputUInt
-  call		Printendl
   
-  call 		PrintRegisters
+ ; call 		PrintRegisters
   
-
+  ;A+(B+C) =D
+  
+  mov	eax, 0h					;zero out the eax register
+  mov	eax,[varB]				;we move the 2 byt value of varB in ax register
+  
+  mov	ebx, 0h					;we need to zero out the ebx register since varC & D ar 4 bytes
+  mov	ebx, [varC]				;mov the value of varC into the ebx register
+  
+  add	eax,ebx					;we add ebx TO eax because we want all math operations to 
+								;finish inside of the eax register
+								
   ;Code ends here
   nop
   mov eax,1 ; Exit system call value
